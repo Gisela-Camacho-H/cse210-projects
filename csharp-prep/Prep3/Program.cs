78ed592dedc88339ac4156b9d9743a9723511608
Gisela Camacho <giscam@Giselas-MacBook-Air.local>
2023-04-25 00:20:49 -0600

    adding prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 51);
+        Console.WriteLine(number);
+        int count = 0;
+        int guessNumber;
+            do
+            {
+                count += 1;
+                Console.Write("What is your guess? (1 - 50)? ");
+                string userInput = Console.ReadLine();
+                guessNumber = int.Parse(userInput);
+                if (guessNumber >= 51)
+                {
+                    Console.WriteLine("The magic number is between 1 - 50");
+                }
+                else if (guessNumber > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guessNumber < number)
+                {
+                    Console.WriteLine("Higher");
+                } else if (guessNumber == number) {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"Number or guesses: {count}");
+                }
+            } while (guessNumber != number);
     }
 }
