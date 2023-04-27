97f34126b6e3c8554cf0bd2449a9894daa1d9e4b
Gisela Camacho <giscam@Giselas-MacBook-Air.local>
2023-04-26 23:20:49 -0600

    adding prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayPersonalMessage();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squaredNumber = SquaredNumber(number);
+        DisplayResult(name, squaredNumber);
+    }
+    static void DisplayPersonalMessage()
+    {
+        Console.Write("Welcome to the Program!\n");
+    }
+    
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userInput = Console.ReadLine();
+        int inputNumber = int.Parse(userInput);
+
+        return inputNumber;
+    }
+    private static int SquaredNumber(int number)
+    {
+        int storeSquaredNumber = number * number;
+        return storeSquaredNumber;
+    }
+    
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
     }
 }
