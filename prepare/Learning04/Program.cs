e89f1766e32a2722b22473cb186ba981d412400b
Gisela Camacho <giscam@Giselas-MacBook-Air.local>
2023-06-01 23:10:29 -0600

    implement learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment myAssignment = new Assignment();
+        myAssignment.SetTopic("Multiplication");
+        myAssignment.SetStudentName("Samuel Bennett");
+        
+        Console.WriteLine(myAssignment.GetSummary());
+        Console.WriteLine("");
+
+        MathAssignment myMathAssingment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 9 - 19");
+        Console.WriteLine(myMathAssingment.GetSummary());
+        Console.WriteLine(myMathAssingment.GetHomeworkList());
+        Console.WriteLine("");
+
+        WritingAssignment myWritingAssingment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(myWritingAssingment.GetSummary());
+        Console.WriteLine(myWritingAssingment.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/math_assignment.cs
@@ -0,0 +1,44 @@
+public class MathAssignment : Assignment
+
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public MathAssignment()
+    {
+    }
+
+    public string GetTextbookSection()
+    {
+        return _textbookSection;
+    }
+
+    public void SetTextbookSection(string textbookSection)
+    {
+        _textbookSection = textbookSection;
+    }
+
+    public string GetProblems()
+    {
+        return _problems;
+    }
+
+    public void SetProblems(string problems)
+    {
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"{_textbookSection} {_problems}";
+    }
+
+}
